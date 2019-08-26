4637c9aceff1a6d6e4985b198559537066753345
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-26 16:01:51 -0700

    have post reducers but at least 2 of them dont work

--- src/reducers/postReducers.js
+++ src/reducers/postReducers.js
@@ -1,11 +1,22 @@
-import { /*put actions here*/ } from '../actions/postActions';
+import { CREATE_POST, UPDATE_POST, DELETE_POST } from '../actions/postActions';
 
 const initialState = {
   posts: [],
 };
 
-function reducer() {
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case CREATE_POST:
+      return { ...state, title: 'here is title', body: 'here is body' };
+    case UPDATE_POST:
+      return { ...state, nofuckingclue };
+    case DELETE_POST:
+      return { ...state, delete the post! }
+    default:
+      return state;
+  }
 
 }
 
 export default reducer;
+
